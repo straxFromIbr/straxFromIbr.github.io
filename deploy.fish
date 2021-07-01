45eab8e60cc983b6a4e0c9e1ed6a2738d2a2dab2
straxFromIbr <haga.yuya2.2016@gmail.com>
2021-07-02 08:46:27 +0900

    2021年 7月 2日 金曜日 08時46分27秒 JST

--- deploy.fish
+++ deploy.fish
@@ -1,7 +1,8 @@
 #! /opt/homebrew/bin/fish
 
 cp config.toml config.toml.backup
-cat config.toml | python -c 'import sys; import toml; dic=toml.load(sys.stdin); s="Build:".join([dic["author"]["bio"].split("Build:")[0]+"<br>","'(date '+%Y/%m/%d %H:%M')'"]); dic["author"]["bio"]=s;f=open("config.toml","w");toml.dump(dic,f);f.close()'
+#cat config.toml | python -c 'import sys; import toml; dic=toml.load(sys.stdin); s="Build:".join([dic["author"]["bio"].split("Build:")[0]+"<br>","'(date '+%Y/%m/%d %H:%M')'"]); dic["author"]["bio"]=s;f=open("config.toml","w");toml.dump(dic,f);f.close()'
+cat config.toml | python -c 'import sys; import toml; dic=toml.load(sys.stdin); s="Build:".join([dic["author"]["bio"].split("Build:")[0],"'(date '+%Y/%m/%d %H:%M')'_"]); dic["author"]["bio"]=s;f=open("config.toml","w");toml.dump(dic,f);f.close()'
 hugo -D --buildFuture
 git add -A
 git commit -m (date)
