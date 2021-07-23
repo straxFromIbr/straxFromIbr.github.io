d60ed9d11431f47dd987fa175a2473f0cd5ecee9
straxFromIbr <haga.yuya2.2016@gmail.com>
2021-07-23 10:47:46 +0900

    2021年 7月23日 金曜日 10時47分46秒 JST

--- makepost.fish
+++ makepost.fish
@@ -4,6 +4,6 @@ hugo new "post/$postname.md"
 mkdir ./content/images/$postname
 
 sed -i "" -e '12s/^/'"thumbnailImage: \/images\/$postname\/"'\n/' ./content/post/$postname.md
-
+echo '{{< image classes="fancybox clear fig-100 center" src="/images/'$postname'/" title="">}}' >> ./content/post/$postname.md
 cd ./content/post
 vim $postname.md
