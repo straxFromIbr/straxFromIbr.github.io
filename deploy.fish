73de30bf92f1074a3c0487ca807f06cd6b485aac
straxFromIbr <haga.yuya2.2016@gmail.com>
2021-08-22 17:50:31 +0900

    2021年 8月22日 日曜日 17時50分31秒 JST

--- deploy.fish
+++ deploy.fish
@@ -1,4 +1,5 @@
 #! /opt/homebrew/bin/fish
+#
 
 cp config.toml config.toml.backup
 cat config.toml | /Users/hagayuya/miniforge3/bin/python3 -c 'import sys; import toml; dic=toml.load(sys.stdin); s="Build:".join([dic["author"]["bio"].split("Build:")[0],"'(date '+%Y/%m/%d %H:%M')'_"]); dic["author"]["bio"]=s;f=open("config.toml","w");toml.dump(dic,f);f.close()'
